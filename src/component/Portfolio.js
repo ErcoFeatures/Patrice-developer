b9489815f48cec8f9b1dd9c7f807334e422955fb
Patrice Diouf <Patrice.diouf@gartner.com>
2021-02-07 18:41:31 -0500

    update logo and avatar

--- src/component/Portfolio.js
+++ src/component/Portfolio.js
@@ -50,10 +50,25 @@ const Card = ({project}) => {
 const Portfolio = ({ data }) => {
 
   if (data) {
-    var projects = data.projects.map((project, index)=> {
-      var projectImage = "images/portfolio/" + project.image;
+    var projects = data.projects.map((projects, index)=> {
+      var projectImage = "images/portfolio/" + projects.image;
       return (
-        <Card key={index} project={project}/>
+        <div key={projects.title} className="columns portfolio-item">
+          <div className="item-wrap">
+            <a href={projects.url} title={projects.title}>
+              <img alt={projects.title} src={projectImage} />
+              <div className="overlay">
+                <div className="portfolio-item-meta">
+                  <h5>{projects.title}</h5>
+                  <p>{projects.category}</p>
+                </div>
+              </div>
+              <div className="link-icon">
+                <i className="fa fa-link"></i>
+              </div>
+            </a>
+          </div>
+        </div>
       );
     });
   }
