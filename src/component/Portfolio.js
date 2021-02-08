4a3a3ec98bc9f272eb45c1a1b0651a790a01e66e
Patrice Diouf <Patrice.diouf@gartner.com>
2021-02-07 20:40:29 -0500

    improve

--- src/component/Portfolio.js
+++ src/component/Portfolio.js
@@ -2,7 +2,7 @@ import React, {useState} from "react";
 import {Button} from "@material-ui/core";
 import ReactCardFlip from "react-card-flip";
 import Grid from "@material-ui/core/Grid";
-const Card = ({project}) => {
+const Card = ({project, projectImageFront , projectImageBack}) => {
   const [isFlipped, setIsFlipped] = useState(false);
 
   const doFlip = () => {
@@ -16,64 +16,78 @@ const Card = ({project}) => {
   }
 
   return (
-      <div className="columns project__card" onMouseEnter={doFlip} onMouseLeave={doFlap}>
-        <div className="item-wrap">
+      // <div className="columns project__card" onMouseEnter={doFlip} onMouseLeave={doFlap}>
+      //   <div className="item-wrap">
 
-          <ReactCardFlip isFlipped={isFlipped}>
-            <div className="project__card">
-              <div className='project__header front_side' style={{backgroundColor: project.bgColor}}>
-                {project.headerImage!=="" && <img width="60" height="70" src={project.headerImage} alt=''/>}
-              </div><div className='project__skills'>
+      //     <ReactCardFlip isFlipped={isFlipped}>
+      //       <div className="project__card">
+      //         <div className='project__header front_side' style={{backgroundColor: project.bgColor}}>
+      //           {project.headerImage!=="" && <img width="60" height="70" src={project.headerImage} alt=''/>}
+      //         </div><div className='project__skills'>
+      //         {
+      //           project.skills.map((el,index) => <div  key={index} className="skill">{el.name} </div>)
+      //         }
+      //       </div>
+      //       </div>
+      //       <div className="project__card back_side" style={{background:project.backColor}}>
+      //         <div className='project__header'>
+      //           <div className="heading">{project.title}</div>
+      //         </div>
+      //         <div className='project__description'>
+      //           {project.description}
+      //         </div>
+      //         <div className="row__button">
+      //           <Button className="button" onClick={()=>goToLink(project.gotoLink)} >{project.buttonLabel}</Button>
+      //         </div>
+      //       </div>
+      //     </ReactCardFlip>
+      //   </div>
+      // </div>
+      <div key={project.title} className="columns portfolio-item"  onMouseEnter={doFlip} onMouseLeave={doFlap}>
+        <ReactCardFlip isFlipped={isFlipped}>
+          <div className="item-wrap">
+              <div className="img__header">
+                <img alt={project.title} src={projectImageFront} />
+              </div>
+            
+                <div className="portfolio-item-meta">
+                  <h5>{project.title}</h5>
+                  <p>{project.category}</p>
+                </div>
+              <div className='project__skills'>
               {
                 project.skills.map((el,index) => <div  key={index} className="skill">{el.name} </div>)
               }
             </div>
-            </div>
-            <div className="project__card back_side" style={{background:project.backColor}}>
-              <div className='project__header'>
-                <div className="heading">{project.title}</div>
+          </div>
+          <div className="item-wrap">
+            <div className="img__header">
+                <img alt={project.title} src={projectImageBack} />
               </div>
+                <div className="portfolio-item-meta">
+                  <h5>{project.title}</h5>
+                  <p>{project.category}</p>
+                </div>
               <div className='project__description'>
-                {project.description}
-              </div>
-              <div className="row__button">
-                <Button className="button" onClick={()=>goToLink(project.gotoLink)} >{project.buttonLabel}</Button>
-              </div>
-            </div>
+                 {project.description}
+               </div>
+               <div className="row__button">
+                 <Button className="button" onClick={()=>goToLink(project.url)} >{project.buttonLabel}</Button>
+               </div>
+          </div>
           </ReactCardFlip>
         </div>
-      </div>
-
   )
 }
 
 const Portfolio = ({ data }) => {
 
   if (data) {
-    var projects = data.projects.map((projects, index)=> {
-      var projectImage = "images/portfolio/" + projects.imageFront;
+    var projects = data.projects.map((project, index)=> {
+      var projectImageFront = "images/portfolio/" + project.imageFront;
+      var projectImageBack = "images/portfolio/" + project.imageBack;
       return (
-        <div key={projects.title} className="columns portfolio-item">
-          <div className="item-wrap">
-            <a href={projects.url} title={projects.title}>
-              <img alt={projects.title} src={projectImage} />
-              <div className="overlay">
-                <div className="portfolio-item-meta">
-                  <h5>{projects.title}</h5>
-                  <p>{projects.category}</p>
-                </div>
-              </div>
-              <div className="link-icon">
-                <i className="fa fa-link"></i>
-              </div>
-              <div className='project__skills'>
-              {
-                projects.skills.map((el,index) => <div  key={index} className="skill">{el.name} </div>)
-              }
-            </div>
-            </a>
-          </div>
-        </div>
+        <Card project={project} projectImageFront={projectImageFront} projectImageBack={projectImageBack} />
       );
     });
   }
